6f489091c708bc8d44279f5a6dc40dafbd311c10
marvin <marvin@galaxyone.space>
2024-01-13 19:03:26 -0500

    update pet ids

--- code-server-docker.tf
+++ code-server-docker.tf
@@ -21,7 +21,7 @@ resource "random_integer" "code_server_port" {
 
 resource "onepassword_item" "cs_sudo_login" {
   vault    = local.vault_id
-  title    = "code_server_${random_pet.code_server_name} Sudo"
+  title    = "code_server_${random_pet.code_server_name.id} Sudo"
   category = "login"
   username = "root"
   password_recipe {
@@ -32,7 +32,7 @@ resource "onepassword_item" "cs_sudo_login" {
 
 resource "docker_container" "codeserver_container" {
   image = docker_image.code-server-img.image_id
-  name  = random_pet.code_server_name
+  name  = random_pet.code_server_name.id
   entrypoint = [
     "/bin/bash",
     "-c",
